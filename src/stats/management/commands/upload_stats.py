920f13fa7052f08d177a02c12651325edbedf86a
Jonas Obrist <jonas.obrist@divio.ch>
2011-10-31 18:24:36 +0100

    renamed

--- src/stats/management/commands/upload_stats.py
+++ src/stats/management/commands/upload_stats.py
@@ -6,10 +6,9 @@ import subprocess
 
 class Command(BaseCommand):
     args = '<project-slug>'
-    help = 'Closes the specified poll for voting'
+    help = 'Uploads stats'
 
     def handle(self, slug, **options):
         repo = Repository.objects.get(slug=slug)
-        repo.build()
         subprocess.check_call(['scp', repo.repodatapath, 'vcs@ssh.ep.io:gitstats/repodata/%s.js' % repo.slug])
         subprocess.check_call(['epio', 'django', 'load_stats', repo.slug, '../data/repodata/%s.js' % repo.slug])
