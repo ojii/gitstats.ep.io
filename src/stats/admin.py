10468262dca3f34f13c3886490e41c2ef37b5bca
Jonas Obrist <jonas.obrist@divio.ch>
2011-10-31 11:17:22 +0100

    nicer admin? nicer admin!

--- src/stats/admin.py
+++ src/stats/admin.py
@@ -2,4 +2,7 @@
 from django.contrib import admin
 from stats.models import Repository
 
-admin.site.register(Repository, readonly_fields=['slug', 'built'])
+admin.site.register(Repository,
+    readonly_fields=['slug', 'built'],
+    list_display=['__unicode__', 'built', 'autobuild']
+)
