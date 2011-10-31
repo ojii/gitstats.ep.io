6cec9693601572e7411959e9aea852a1e537f737
Jonas Obrist <jonas.obrist@divio.ch>
2011-10-31 16:43:22 +0100

    cached a bit too hard

--- src/git_statistics/utils.py
+++ src/git_statistics/utils.py
@@ -1,25 +1,32 @@
 # -*- coding: utf-8 -*-
 import datetime
 
+
+
 def simple_method_cacher(method):
-    method.cache = {}
     def _decorator(self, *args):
+        if not hasattr(self, '_%s_cache' % method.__name__):
+            setattr(self, '_%s_cache' % method.__name__, {})
+        cache = getattr(self, '_%s_cache' % method.__name__)
         try:
-            value = method.cache[args]
+            value = cache[self][args]
         except KeyError:
             value = method(self, *args)
-            method.cache[args] = value
+            cache[self][args] = value
         return value
     _decorator.__name__ = 'simple_method_cacher(%s)' % method.__name__
     return _decorator
 
 def complex_method_cacher(method):
-    method.cache = None
-    method.default = None
     def _decorator(self, *args):
-        if method.cache is None:
-            method.cache, method.default = method(self, *args)
-        return method.cache.get(args, method.default)
+        if not hasattr(self, '_%s_cache' % method.__name__):
+            cache, default = method(self, *args)
+            setattr(self, '_%s_cache' % method.__name__, cache)
+            setattr(self, '_%s_cache_default' % method.__name__, default)
+        else:
+            cache = getattr(self, '_%s_cache' % method.__name__)
+            default = getattr(self, '_%s_cache_default' % method.__name__)
+        return cache.get(args, default)
     _decorator.__name__ = 'complex_method_cacher(%s)' % method.__name__
     return _decorator
 
